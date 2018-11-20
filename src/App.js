36bd37033cb29789e1603583b6ffd4572608514b
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-11-20 17:37:41 -0500

    Feat(Frontend): Start new query and select start table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './MakeQuery'
-import MakeQuery from './MakeQuery';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MakeQuery from "./MakeQuery";
+import StartQuery from "./StartQuery";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <MakeQuery />
+          <Router>
+            <Switch>
+              <Route path="/makeQuery" component={MakeQuery} />
+              <Route path="/startQuery" component={StartQuery} />
+              <Route component={StartQuery} />{" "}
+              {/*ADJUST AS APPLICABLE Default route*/}
+            </Switch>
+          </Router>
         </header>
       </div>
     );
