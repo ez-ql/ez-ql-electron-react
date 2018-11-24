cc57f0c1863cc5c1db01184258afcbf86345c0a8
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2018-11-23 20:24:14 -0500

    #36: feat(front) - change table and field presentation to human-readable format

--- src/Selector/Selector.js
+++ src/Selector/Selector.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Flickity from 'flickity';
+import React, { Component } from "react";
+import Flickity from "flickity";
 
 const itemStyle = {
-  width: '30%',
-  height: '260px',
-  background: 'lightgrey',
-  borderRadius: '5px',
-  margin: '10px'
+  width: "30%",
+  height: "260px",
+  background: "lightgrey",
+  borderRadius: "5px",
+  margin: "10px"
 };
 
 export default class Selector extends Component {
@@ -14,9 +14,9 @@ export default class Selector extends Component {
     super(props);
     this.state = {
       selectedIndex: 0,
-      subSelector: false,
-    }
-    this.appendChange=this.appendChange.bind(this)
+      subSelector: false
+    };
+    this.appendChange = this.appendChange.bind(this);
   }
 
   // flkty = null;
@@ -29,55 +29,55 @@ export default class Selector extends Component {
     if (subSelector) {
       this.setState({
         selectedIndex: items.length,
-        subSelector: true,
+        subSelector: true
       });
     }
 
-    this.initFlickity()
+    this.initFlickity();
     // setTimeout(this.initFlickity, 0);
   }
-
 
   componentWillUnmount() {
     if (this.flkty) {
       this.flkty.destroy();
     }
-  };
+  }
 
-  componentDidUpdate(prevProps, prevState){
-    if(prevProps.items !== this.props.items){
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.items !== this.props.items) {
       this.appendChange();
     }
   }
 
   initFlickity = () => {
     const options = {
-      cellSelector: '.item',
+      cellSelector: ".item",
       contain: false,
       initialIndex: this.scrollAt,
       accessibility: false,
       pageDots: false,
       wrapAround: false,
-      prevNextButtons: true,
-    }
+      prevNextButtons: true
+    };
 
     this.flkty = new Flickity(this.wrapper, options);
-    this.flkty.on('dragStart', this.dragStart);
-    this.flkty.on('dragEnd', this.dragEnd);
-    this.flkty.on('scroll', this.onScroll);
-    this.flkty.on('settle', this.onSettle);
-
+    this.flkty.on("dragStart", this.dragStart);
+    this.flkty.on("dragEnd", this.dragEnd);
+    this.flkty.on("scroll", this.onScroll);
+    this.flkty.on("settle", this.onSettle);
   };
 
   onSettle = () => {
     const selectedIndex = this.flkty.selectedIndex;
     if (this.state.selectedIndex !== selectedIndex) {
       this.setState({
-        selectedIndex: selectedIndex,
+        selectedIndex: selectedIndex
       });
     }
-    const modelName = this.flkty.selectedSlide.cells[0].element.innerText.split(' ')[0]
-    this.props.selectedSlide(modelName)
+    const modelName = this.flkty.selectedSlide.cells[0].element.innerText.split(
+      " "
+    )[0];
+    this.props.selectedSlide(modelName);
   };
 
   prevScroll = false;
@@ -90,19 +90,19 @@ export default class Selector extends Component {
     let direction = false;
 
     if (this.prevScroll !== false) {
-      direction = (scroll < this.prevScroll) ? 'right' : 'left';
+      direction = scroll < this.prevScroll ? "right" : "left";
     }
 
     const boundaries = {
-      left: this.scrollAt * (this.state.selectedIndex),
-      right: (this.scrollAt * (this.state.selectedIndex + 1)) - (this.scrollAt / 2),
-    }
+      left: this.scrollAt * this.state.selectedIndex,
+      right: this.scrollAt * (this.state.selectedIndex + 1) - this.scrollAt / 2
+    };
 
     // When you move the slider to the left
-    if (scroll > boundaries.right && direction === 'left') {
-      const next = this.state.selectedIndex += 1;
+    if (scroll > boundaries.right && direction === "left") {
+      const next = (this.state.selectedIndex += 1);
       this.setState({
-        selectedIndex: next,
+        selectedIndex: next
       });
 
       if (sub) {
@@ -111,10 +111,14 @@ export default class Selector extends Component {
     }
 
     // When you move the slider to the right
-    if (scroll < boundaries.left && direction === 'right' && this.state.selectedIndex !== 0) {
-      const prev = this.state.selectedIndex -= 1;
+    if (
+      scroll < boundaries.left &&
+      direction === "right" &&
+      this.state.selectedIndex !== 0
+    ) {
+      const prev = (this.state.selectedIndex -= 1);
       this.setState({
-        selectedIndex: prev,
+        selectedIndex: prev
       });
 
       if (sub) {
@@ -130,7 +134,7 @@ export default class Selector extends Component {
 
     if (updateState) {
       this.setState({
-        selectedIndex: index,
+        selectedIndex: index
       });
     }
   };
@@ -149,32 +153,53 @@ export default class Selector extends Component {
 
   dragEnd = e => {
     this.dragStarted = false;
-    const modelName = this.flkty.selectedSlide.cells[0].element.innerText.split(' ')[0]
-    this.props.selectedSlide(modelName)
+    const modelName = this.flkty.selectedSlide.cells[0].element.innerText.split(
+      " "
+    )[0];
+    this.props.selectedSlide(modelName);
   };
 
-  appendChange(){
+  appendChange() {
     if (this.flkty) {
       this.flkty.destroy();
       this.initFlickity();
     }
   }
 
+  //func to format name of selected table in carousel
+  //takes str, not array, like in rest of query-builder
+  formatTableAndFieldNames = str => {
+    let mod;
+    if (str.includes("_")) {
+      let [first, second] = str.split("_");
+      mod = `${first.charAt(0).toUpperCase()}${first.slice(1)} ${second
+        .charAt(0)
+        .toUpperCase()}${second.slice(1)}`;
+    } else {
+      mod = `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+    }
+    return mod;
+  };
+
   render() {
     const { items } = this.props;
     return (
       <div>
-        <div ref={ch => this.wrapper = ch} >
-          {items.map(item =>
-            <div  style={itemStyle} className="item">
-              <div className="inner Color">{`${item.model_name} table`}</div>
-              {
-                item.fields.map(category => <div className="inner">{category}</div>)
-              }
+        <div ref={ch => (this.wrapper = ch)}>
+          {items.map(item => (
+            <div style={itemStyle} className="item">
+              <div className="inner Color">{`${this.formatTableAndFieldNames(
+                item.model_name
+              )} Table`}</div>
+              {item.fields.map(category => (
+                <div className="inner">
+                  {this.formatTableAndFieldNames(category)}
+                </div>
+              ))}
             </div>
-          )}
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
