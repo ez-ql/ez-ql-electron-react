ab0b5694b396f83ed4533ffb7b513b9d5369ab27
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-11-24 22:56:14 -0500

    commit before switching branch

--- src/FieldButtons.js
+++ src/FieldButtons.js
@@ -2,17 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
-import Button from '@material-ui/core/Button';
-
+import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
-  lightTooltip: {
-    background: theme.palette.common.white,
-    color: theme.palette.text.primary,
-    boxShadow: theme.shadows[1],
-    fontSize: 11
+  // lightTooltip: {
+  //   background: theme.palette.common.white,
+  //   color: theme.palette.text.primary,
+  //   boxShadow: theme.shadows[1],
+  //   fontSize: 15
+  // },
+  tooltip: {
+    fontSize: "1em"
   },
   arrowPopper: {
+    textAlign: "center",
     '&[x-placement*="bottom"] $arrowArrow': {
       top: 0,
       left: 0,
@@ -24,43 +27,6 @@ const styles = theme => ({
         borderColor: `transparent transparent ${
           theme.palette.grey[700]
         } transparent`
-      }
-    },
-    '&[x-placement*="top"] $arrowArrow': {
-      bottom: 0,
-      left: 0,
-      marginBottom: "-0.9em",
-      width: "3em",
-      height: "1em",
-      "&::before": {
-        borderWidth: "1em 1em 0 1em",
-        borderColor: `${
-          theme.palette.grey[700]
-        } transparent transparent transparent`
-      }
-    },
-    '&[x-placement*="right"] $arrowArrow': {
-      left: 0,
-      marginLeft: "-0.9em",
-      height: "3em",
-      width: "1em",
-      "&::before": {
-        borderWidth: "1em 1em 1em 0",
-        borderColor: `transparent ${
-          theme.palette.grey[700]
-        } transparent transparent`
-      }
-    },
-    '&[x-placement*="left"] $arrowArrow': {
-      right: 0,
-      marginRight: "-0.9em",
-      height: "3em",
-      width: "1em",
-      "&::before": {
-        borderWidth: "1em 0 1em 1em",
-        borderColor: `transparent transparent transparent ${
-          theme.palette.grey[700]
-        }`
       }
     }
   },
@@ -115,15 +81,15 @@ class FieldButtons extends React.Component {
               type === "string"
                 ? `"${thisField.field_example}"`
                 : thisField.field_example;
+
             return (
               <div>
                 <Tooltip
                   title={
                     <React.Fragment>
-                        {
-                            //line break not working
-                        }
-                      {`e.g: ${example}\nType: ${type}`}
+                      {`e.g: ${example}`}
+                      <br />
+                      {`Type: ${type}`}
                       <span
                         className={classes.arrowArrow}
                         ref={this.handleArrowRef}
@@ -143,7 +109,6 @@ class FieldButtons extends React.Component {
                   }}
                 >
                   <Button
-                    className="Button"
                     type="submit"
                     name="fields"
                     value={field}
