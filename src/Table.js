9c3944f28557a6a974fe6741753b7c3220b3fcea
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-11-26 16:53:26 -0500

    small changes

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MUIDataTable from "mui-datatables";
 import { formatNames } from "./MakeQuery.js";
+const electron = window.require("electron");
 
 const Table = props => {
   const { data } = props;
@@ -10,18 +11,37 @@ const Table = props => {
     originalColumnNames = Object.keys(data[0]);
     prettyColumnNames = Object.values(formatNames(originalColumnNames));
   }
+
+  const globalObj = electron.remote.getGlobal("sharedObj");
+  const currQuery = globalObj.currQuery;
+  const models = globalObj.models;
+  let selectedFields = [];
+  currQuery.selectedModelsAndFields.forEach(model => {
+    const modelDetail = models.filter(
+      globalModel => globalModel.model_name === model.model_name
+    );
+    modelDetail[0].fields.forEach(globalField => {
+      if (model.fields.includes(globalField.field_name)) {
+        selectedFields.push(globalField);
+      }
+    });
+  });
+
+  console.log("*****selectedFields*******", selectedFields);
+
   const rowValuesOnly = data.map(row => Object.values(row));
 
-  // const options = {
-  //   //tbd
-  // };
+  const options = {
+    responsive: "scroll"
+  };
 
   return (
     <MUIDataTable
+      className="table"
       title={"Preview results"}
       data={rowValuesOnly}
       columns={prettyColumnNames}
-      //options={options}
+      options={options}
     />
   );
 };
